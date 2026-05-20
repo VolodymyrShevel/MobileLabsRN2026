4186a105f43b2fa051c28924d7d71822ca7ddf23
VolodymyrShevel <v.shevel@gmail.com>
2026-05-20 22:39:58 +0300

    Fix embedded repo

---
+++ lab01/Lab1/components/BottomLabel.tsx
@@ -0,0 +1,16 @@
+import { StyleSheet, Text } from "react-native";
+
+export const BottomLabel = () => {
+  return <Text style={styles.bottomLabel}>Шевель Володимир, ЗІПЗ-22-1</Text>;
+};
+
+const styles = StyleSheet.create({
+  bottomLabel: {
+    position: "absolute",
+    bottom: 10,
+    fontSize: 14,
+    color: "gray",
+    textAlign: "center",
+    alignSelf: 'center'
+  },
+});
